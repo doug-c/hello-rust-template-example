f1df3d1fc8a021a14ca38585f5f40a1a1b20c600
Doug Copeland <douglas.copeland@gmail.com>
2024-03-08 14:36:25 -0500

    cleanup

--- hello-marco/target/debug/deps/hello_marco-27e0d58ff2855e95.d
+++
@@ -1,8 +0,0 @@
-/workspaces/hello-rust-template-example/hello-marco/target/debug/deps/hello_marco-27e0d58ff2855e95.rmeta: src/main.rs Cargo.toml
-
-/workspaces/hello-rust-template-example/hello-marco/target/debug/deps/hello_marco-27e0d58ff2855e95.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
